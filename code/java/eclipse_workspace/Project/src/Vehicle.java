de7b314106e3d33446bc9226433a8e6f96fc48bc
JohnReborn <jaketj554@gmail.com>
2018-03-01 19:20:23 +0000

    yup
    so guys its that time of the month again..yup my peroid. noo not menstrual cycle but the peroid "." which means the end or to stop. anyways enjoy

--- code/java/eclipse_workspace/Project/src/Vehicle.java
+++ code/java/eclipse_workspace/Project/src/Vehicle.java
@@ -3,4 +3,21 @@ public class Vehicle {
 	public int row, col;
 	public int destRow, destCol;
 	public boolean occupied;
+	
+	
+	public Vehicle()
+	{
+		this.row=0;
+		this.col=0;
+	}
+	
+	public void Vehicle(int row, int col)
+	{
+		this.row=row;
+		this.col=col;
+	}
+	
+	
+	
+	
 }
